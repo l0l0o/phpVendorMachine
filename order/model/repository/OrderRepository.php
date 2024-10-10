964849b98742b61af876c2c8da2888758f288f7e
Loïc CAILLE <loccaille@gmail.com>
2024-10-10 11:50:32 +0200

    rectification erreur chemin

--- order/model/repository/OrderRepository.php
+++ order/model/repository/OrderRepository.php
@@ -1,29 +1,28 @@
-<?php
-
-require_once './model/entity/Order.php';
-
+<?php 
+require_once './order/model/entity/Order.php';
 
 class OrderRepository {
 
-	// permet d'initialiser la session (session_start())
-	// à chaque fois qu'on instancie la classe OrderRepository
-	// sans l'initialisation de la session, on ne peut pas 
-	// utiliser correctement la session ($_SESSION)
-	public function __construct() {
-		session_start();
-	}
+    // permet d'initialiser la session 
+    // à chaque fois qu'on instancie la classe OrderRepository
 
-	public function persist(Order $order): Order {
-		$_SESSION['order'] = $order;
-		return $order;
-	}
+    // sans l'initialisation de la session, on ne peut pas
+    // utiliser correctement la session ($_SESSION)
+    public function __construct() {
+        session_start();
+    }
 
-	public function find(): ?Order {
-		if (!isset($_SESSION['order'])) {
-			return null;
-		}
+    public function persist(Order $order): ?Order {
+        $_SESSION['order'] = $order;
+        return $order;
+    }
 
-		return $_SESSION['order'];
-	}
+    public function find() {
+        if(!isset($_SESSION['order'])) {
+            return null;
+        }
+
+        return $_SESSION['order'];
+    }
 
 }
